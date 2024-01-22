72f605a959270ae1db8baf174e1dd6ad33cd899a
Fitsum Abebe <engfitsum@gmail.com>
2024-01-22 12:14:19 +0300

    lst

--- src/libs/actions/Report.ts
+++ src/libs/actions/Report.ts
@@ -916,7 +916,7 @@ function expandURLPreview(reportID: string, reportActionID: string) {
 }
 
 /** Marks the new report actions as read */
-function readNewestAction(reportID: string) {
+function readNewestAction(reportID: string, shouldEmitEvent = true) {
     const lastReadTime = DateUtils.getDBTime();
 
     const optimisticData: OnyxUpdate[] = [
@@ -940,6 +940,12 @@ function readNewestAction(reportID: string) {
     };
 
     API.write('ReadNewestAction', parameters, {optimisticData});
+
+    if (!shouldEmitEvent) {
+        return;
+    }
+
+    DeviceEventEmitter.emit(`readNewestAction_${reportID}`, lastReadTime);
 }
 
 /**
