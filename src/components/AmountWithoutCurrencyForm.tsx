c2015444ce06a45e6b554b2bfd41b7cdf58cafaf
FitseTLT <engfitsum@gmail.com>
2024-09-28 03:27:21 +0300

    fix flickering

--- src/components/AmountWithoutCurrencyForm.tsx
+++ src/components/AmountWithoutCurrencyForm.tsx
@@ -1,5 +1,6 @@
-import React, {useCallback, useMemo} from 'react';
+import React, {useCallback, useMemo, useRef, useState} from 'react';
 import type {ForwardedRef} from 'react';
+import {NativeSyntheticEvent, TextInputSelectionChangeEventData} from 'react-native';
 import useLocalize from '@hooks/useLocalize';
 import {addLeadingZero, replaceAllDigits, replaceCommasWithPeriod, stripSpacesFromAmount, validateAmount} from '@libs/MoneyRequestUtils';
 import CONST from '@src/CONST';
@@ -14,6 +15,8 @@ type AmountFormProps = {
     onInputChange?: (value: string) => void;
 } & Partial<BaseTextInputProps>;
 
+const decimal = 2;
+
 function AmountWithoutCurrencyForm(
     {value: amount, onInputChange, inputID, name, defaultValue, accessibilityLabel, role, label, ...rest}: AmountFormProps,
     ref: ForwardedRef<BaseTextInputRef>,
@@ -21,7 +24,8 @@ function AmountWithoutCurrencyForm(
     const {toLocaleDigit} = useLocalize();
 
     const currentAmount = useMemo(() => (typeof amount === 'string' ? amount : ''), [amount]);
-
+    const [maxLength, setMaxLength] = useState<number | undefined>(undefined);
+    const amountRef = useRef();
     /**
      * Sets the selection and the amount accordingly to the value passed to the input
      * @param newAmount - Changed amount from user input
@@ -33,20 +37,32 @@ function AmountWithoutCurrencyForm(
             const newAmountWithoutSpaces = stripSpacesFromAmount(newAmount);
             const replacedCommasAmount = replaceCommasWithPeriod(newAmountWithoutSpaces);
             const withLeadingZero = addLeadingZero(replacedCommasAmount);
-            if (!validateAmount(withLeadingZero, 2)) {
+            if (!validateAmount(withLeadingZero, decimal)) {
                 return;
             }
+            amountRef.current = withLeadingZero;
             onInputChange?.(withLeadingZero);
         },
         [onInputChange],
     );
-
     const formattedAmount = replaceAllDigits(currentAmount, toLocaleDigit);
 
     return (
         <TextInput
             value={formattedAmount}
             onChangeText={setNewAmount}
+            onSelectionChange={(e: NativeSyntheticEvent<TextInputSelectionChangeEventData>) => {
+                const selection = e.nativeEvent.selection;
+                const am = amountRef.current ?? currentAmount;
+                const formattedAmount = replaceAllDigits(am, toLocaleDigit);
+
+                if (!formattedAmount.includes('.') || selection.start !== selection.end || selection.start <= formattedAmount.indexOf('.')) {
+                    setMaxLength(undefined);
+                    return;
+                }
+                setMaxLength(formattedAmount.split('.')?.[0].length + 1 + decimal);
+            }}
+            maxLength={maxLength}
             inputID={inputID}
             name={name}
             label={label}
